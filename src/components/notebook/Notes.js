45ee5251ca8fc7a090ec4eb75bc385d9aa7bcca2
Orman, Clayton <CDO8081@yum.com>
2019-07-26 12:36:02 -0400

    Add ability to create new note

--- src/components/notebook/Notes.js
+++ src/components/notebook/Notes.js
@@ -36,39 +36,28 @@ class Notes extends Component {
             });
     }
 
-    // renderNote(taskId) {
-    //     if (!this.state.notes.length > 0) return;
-
-    //     let note = this.state.notes.find(note => note.task_id === taskId);
-
-    //     console.log('note: ', note);
-    //     return note ? 
-    //         <textarea id="page" cols="30" rows="10" placeholder="well...start note taking" value={note.content} onChange={(e) => this.setState({ activeNote: e.target.value })}></textarea> :
-    //         <textarea id="page" cols="30" rows="10" placeholder="well...start note taking" onChange={(e) => this.setState({ activeNote: e.target.value })}></textarea>;
-    // }
-
     handleNoteChange = (taskId, e) => {
         this.setState({[taskId]: e.target.value});
     }
 
     createUpdateNote(taskId, event) {
+        //TODO: this only works currently while creating new notes.
+        //Update to PATCH existing notes.
         event.preventDefault();
+        console.log(`Creating the following note: `, this.state[taskId]);
+
+        let options = this.props.getRequestOptions('POST');
+        options.body = JSON.stringify({ task_id: parseInt(taskId), content: this.state[taskId] });
+
+        this.props.callWunderlistApi(notesApi, options) 
+            .then(() => {
+                this.fetchNotes();
+            })
+            .catch(() => {
+                console.log(`Something went wrong while trying to create a note.`);
+            });
+
         console.log(`Note with parent task ${taskId} saved!`);
-        console.log(`current  state: `, this.state[event.target.name]);
-
-        // let options = this.props.getRequestOptions('POST');
-        // options.body = JSON.stringify({ task_id: parseInt(taskId), content: 'Test Note' });
-
-        // console.log(`Creating note with content`);
-        // this.props.callWunderlistApi(notesApi, options)
-        //     .then(() => {
-        //         this.fetchNotes();
-        //     })
-        //     .catch(() => {
-        //         console.log(`Something went wrong while trying to create a note.`);
-        //     });
-
-
         this.setState({ showButton: false });
     }
 
