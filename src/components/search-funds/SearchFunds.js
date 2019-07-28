c11642f88300942e18728fa40a3c419c533bceb1
Clayton Orman <corman.dev@gmail.com>
2019-07-28 18:39:00 -0400

    Add chartjs and react-chartjs libraries. Create example line chart

--- src/components/search-funds/SearchFunds.js
+++ src/components/search-funds/SearchFunds.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom';
+import { Line, defaults } from 'react-chartjs-2'
 import StockFinder from './StockFinder';
 import IndexFinder from './IndexFinder';
+
+defaults.global.maintainAspectRatio = false // Don't maintain w/h ratio globally
 
 class SearchFunds extends Component {
     //Container component for searching stocks and indexes
     render() {
+        const data = {
+            labels: [
+              '10/04/2018', '10/05/2018', 
+              '10/06/2018', '10/07/2018', 
+              '10/08/2018', '10/09/2018', 
+              '10/10/2018', '10/11/2018', 
+              '10/12/2018', '10/13/2018', 
+              '10/14/2018', '10/15/2018'
+            ],
+            datasets: [
+              {
+                label: 'Temperature',
+                data: [22,19,27,23,22,24,17,25,23,24,20,19],
+                fill: false,          // Don't fill area under the line
+                borderColor: 'green'  // Line color
+              }
+            ]
+          }
+      
         return (
             <div>
                 <div>
@@ -16,6 +38,11 @@ class SearchFunds extends Component {
                     <Link className='btn btn-primary my-2' to='/search-funds/find-stock'>Search For A Stock</Link>
                     <Link className='btn btn-secondary my-2' to='/search-funds/find-index'>Search For An Index</Link>
                 </div>
+
+
+                <article className="canvas-container">
+                    <Line data={data} />
+                </article>
 
                 <Route path="/search-funds/find-stock" render={() => <StockFinder />} />
                 <Route path="/search-funds/find-index" render={() => <IndexFinder />} />
