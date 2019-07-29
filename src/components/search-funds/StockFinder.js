e1321ce092d00215154bae00cf0e683ad6a7138e
Orman, Clayton <CDO8081@yum.com>
2019-07-29 09:22:57 -0400

    Add dynamic chart implementation

--- src/components/search-funds/StockFinder.js
+++ src/components/search-funds/StockFinder.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Line, defaults } from 'react-chartjs-2';
 
 const url = 'https://financialmodelingprep.com/api/v3/historical-price-full/';
-const timeSeries = 7; //gets last five days
+const timeSeries = 25; //gets last five days
 defaults.global.maintainAspectRatio = false // Don't maintain w/h ratio globally
 
 class StockFinder extends Component {
@@ -14,14 +14,7 @@ class StockFinder extends Component {
             price: 0,
             history: [],
             labels: [],
-            datasets: [
-                {
-                    label: '',
-                    data: [],
-                    fill: false,          // Don't fill area under the line
-                    borderColor: 'green'  // Line color
-                }
-            ]
+            data: []
         }
     }
 
@@ -41,20 +34,48 @@ class StockFinder extends Component {
         return result;
     }
 
+    handleFormSubmit = (event) => {
+        //TODO: refactor and create method.
+        event.preventDefault();
+        const dataValues = [];
+        const labelValues = [];
+        this.fetchStock(this.state.symbol)
+            .then(response => {
+                if (response.Error) {
+                    console.log('Could not complete request. Resetting symbol');
+                    this.setState({ symbol: '', price: 0 });
+                    return;
+                }
+                response.historical.forEach(record => {
+                    dataValues.push(record.close);
+                    labelValues.push(this.formatDate(record.date));
+                })
+                this.setState({ 
+                    history: response.historical, 
+                    symbol: response.symbol, 
+                    price: response.historical[response.historical.length - 1].close,
+                    labels: labelValues,
+                    data: dataValues
+                });
+            })
+            .catch((error) => {
+                console.log('error response: ', error);
+                console.log(`Something went wrong trying to find information for ${this.state.symbol}, please try again.`)
+            });
+    }
+
+    formatDate(date) {
+        var dateToFormat = new Date(date);
+        return `${dateToFormat.getDay()}/${dateToFormat.getMonth()}/${dateToFormat.getFullYear()}`
+    }
+
     render() {
         const data = {
-            labels: [
-              '10/04/2018', '10/05/2018', 
-              '10/06/2018', '10/07/2018', 
-              '10/08/2018', '10/09/2018', 
-              '10/10/2018', '10/11/2018', 
-              '10/12/2018', '10/13/2018', 
-              '10/14/2018', '10/15/2018'
-            ],
+            labels: this.state.labels, 
             datasets: [
               {
-                label: 'Temperature',
-                data: [22,19,27,23,22,24,17,25,23,24,20,19],
+                label: 'Closing Price',
+                data: this.state.data,
                 fill: false,          // Don't fill area under the line
                 borderColor: 'green'  // Line color
               }
@@ -63,28 +84,7 @@ class StockFinder extends Component {
 
         return (
             <div>
-                <form onSubmit={
-                    (event) => {
-                        //TODO: refactor and create method.
-                        event.preventDefault()
-                        this.fetchStock(this.state.symbol)
-                            .then(response => {
-                                if (response.Error) {
-                                    console.log('Could not complete request. Resetting symbol');
-                                    this.setState({ symbol: '', price: 0 });
-                                    return;
-                                }
-                                this.setState({ 
-                                    history: response.historical, 
-                                    symbol: response.symbol, 
-                                    price: response.historical[response.historical.length - 1].close 
-                                });
-                            })
-                            .catch((error) => {
-                                console.log('error response: ', error);
-                                console.log(`Something went wrong trying to find information for ${this.state.symbol}, please try again.`)
-                            });
-                    }}>
+                <form onSubmit={(event) => this.handleFormSubmit(event)}>
                     <label>Name: </label>
                     <input type='text' id='name' value={this.state.symbol} onChange={(event) => {
                         //TODO: dont update on change, only on submit
