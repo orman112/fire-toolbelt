847c9c7ae71fa9f7bcd8e0992a7aaaa1f0e695f6
Orman, Clayton <CDO8081@yum.com>
2019-07-30 17:12:35 -0400

    Center navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <ul className="nav navbar-nav justify-content-center">
+                <ul className="nav navbar-nav mx-auto">
                     <li><Link className="navbar-brand" to="/">Home</Link></li>
                     <li><Link className="navbar-brand" to="/search-funds">Search Funds</Link></li>
                     <li><Link className="navbar-brand" to="/notebook">Notes</Link></li>
